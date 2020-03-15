a5277930a03f20bc403a2fe5dc4a08e56f26fcfa
bamrahp <bamrahprabhjyot99@gmail.com>
2020-03-15 12:50:56 +0530

    first commit

---
+++ AlzheimerMain/__init__.py
@@ -0,0 +1,23 @@
+from PIL import Image 
+from fastai.vision import *
+from fastai.metrics import accuracy
+import pickle
+import os
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+from skimage import io
+from flask import Flask
+import joblib
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+app = Flask(__name__)
+
+app.config["SECRET_KEY"]='DQ4YlKxRuJ6gOPhry8chBw'
+app.config["IMAGE_UPLOADS"]="C:\\Users\\bamra_5waxht9\\Desktop\\Alzheimer\\AlzheimerMain\\static\\img\\uploads"
+app.config["ALLOWED_IMAGE_EXTENSIONS"]=["PNG","JPG","JPEG"]
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///arp.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+from AlzheimerMain import routes
